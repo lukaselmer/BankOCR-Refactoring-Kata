c58dd72a4944f6c92b8f092a654e547c8017dae0
Lukas Elmer <lukas.elmer@gmail.com>
2023-05-13 01:47:48 +0200

    Extract raw line parsing to functions

--- typescript/src/ocr.ts
+++ typescript/src/ocr.ts
@@ -1,23 +1,10 @@
 import { Result, DigitOrInvalid } from './Result'
 import { Numeral, numerals } from './numerals'
 
-export function parse(rawLines: string[]): string[] {
+export function parse(lines: string[]): string[] {
+  const allBlocks = parseLinesToBlocks(lines)
+
   const allResults: string[] = []
-  const lines = []
-  for (let i = 0; i < rawLines.length; i += 4) {
-    lines.push(rawLines.slice(i, i + 4))
-  }
-
-  const allBlocks = []
-  for (let line of lines) {
-    const blocks = []
-    for (let pos = 0; pos < 9; ++pos) {
-      const block = line.map((l) => l.slice(4 * pos, 4 * pos + 4))
-      blocks.push(block)
-    }
-    allBlocks.push(blocks)
-  }
-
   for (let blocks of allBlocks) {
     let result = new Result()
     for (let block of blocks) {
@@ -27,6 +14,33 @@ export function parse(rawLines: string[]): string[] {
     allResults.push(result.stringifyResult())
   }
   return allResults
+}
+
+function parseLinesToBlocks(rawLines: string[]) {
+  const lines = extractLines(rawLines)
+  const allBlocks = extractBlocks(lines)
+  return allBlocks
+}
+
+function extractBlocks(lines: string[][]) {
+  const allBlocks = []
+  for (let line of lines) {
+    const blocks = []
+    for (let pos = 0; pos < 9; ++pos) {
+      const block = line.map((l) => l.slice(4 * pos, 4 * pos + 4))
+      blocks.push(block)
+    }
+    allBlocks.push(blocks)
+  }
+  return allBlocks
+}
+
+function extractLines(rawLines: string[]) {
+  const lines = []
+  for (let i = 0; i < rawLines.length; i += 4) {
+    lines.push(rawLines.slice(i, i + 4))
+  }
+  return lines
 }
 
 function handleNumeralPosition(result: Result, lines: string[]) {
