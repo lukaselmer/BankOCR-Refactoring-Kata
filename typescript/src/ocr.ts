e4e5e785d5295f5f1e4975db6d78813aeee97638
Lukas Elmer <lukas.elmer@gmail.com>
2023-05-13 01:15:36 +0200

    Extract logical lines

--- typescript/src/ocr.ts
+++ typescript/src/ocr.ts
@@ -43,10 +43,15 @@ const NUMERALS = [
 
 export function parse(lines: string[]): string[] {
   const allResults: string[] = []
+  const logicalLines = []
   for (let i = 0; i < lines.length; i += 4) {
+    logicalLines.push(lines.slice(i, i + 4))
+  }
+
+  for (let i = 0; i < logicalLines.length; i += 1) {
     let result = new Result()
     for (let pos = 0; pos < 9; ++pos) {
-      handleNumeralPosition(result, pos, lines, i)
+      handleNumeralPosition(result, pos, lines, i * 4)
     }
 
     allResults.push(result.stringifyResult())
