32f622ddd417de7199f9f05d2ef78017a1c96eb0
Lukas Elmer <lukas.elmer@gmail.com>
2023-05-13 02:00:31 +0200

    Refactor checksum check

--- typescript/src/Result.ts
+++ typescript/src/Result.ts
@@ -10,7 +10,16 @@ export class Result {
   }
 
   private suffix() {
-    return !this.isValid() ? 'ILL' : '   '
+    return !this.isValid() ? 'ILL' : this.invalidChecksum() ? 'ERR' : '   '
+  }
+
+  private invalidChecksum() {
+    // TODO: implement this
+    // account number:  6  6  4  3  7  1  4  9  5
+    // position names:  d9 d8 d7 d6 d5 d4 d3 d2 d1
+    // checksum calculation:
+    // (1*d1+ 2*d2 + 3*d3 + ... + 9*d9) mod 11 = 0
+    return false
   }
 
   private isValid() {
