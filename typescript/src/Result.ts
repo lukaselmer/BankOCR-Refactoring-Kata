ad9314df8d96a5f66412b98c784d6541385a81c8
Lukas Elmer <lukas.elmer@gmail.com>
2023-05-13 02:33:39 +0200

    Implement checksum, correct tests

--- typescript/src/Result.ts
+++ typescript/src/Result.ts
@@ -14,12 +14,11 @@ export class Result {
   }
 
   private invalidChecksum() {
-    // TODO: implement this
-    // account number:  6  6  4  3  7  1  4  9  5
-    // position names:  d9 d8 d7 d6 d5 d4 d3 d2 d1
-    // checksum calculation:
-    // (1*d1+ 2*d2 + 3*d3 + ... + 9*d9) mod 11 = 0
-    return this.joinDigits() === '664371495'
+    const checksum = this.state
+      .filter(numbersOnly)
+      .reverse()
+      .reduce((sum, digit, position) => sum + calculateChecksumPosition({ digit, position }), 0)
+    return checksum % 11 !== 0
   }
 
   private isValid() {
@@ -29,6 +28,23 @@ export class Result {
   private joinDigits() {
     return this.state.join('')
   }
+}
+
+function numbersOnly(digit: DigitOrInvalid): digit is number {
+  return typeof digit === 'number'
+}
+
+/**
+ * Calculate the checksum of a certain account digit
+ *
+ * account number:  6  6  4  3  7  1  4  9  5
+ * position names:  d9 d8 d7 d6 d5 d4 d3 d2 d1
+ * checksum calculation:
+ * (1*d1+ 2*d2 + 3*d3 + ... + 9*d9) mod 11 = 0
+ */
+function calculateChecksumPosition({ digit, position }: { digit: number; position: number }) {
+  // console.log(`${position + 1} * ${digit}`)
+  return (position + 1) * digit
 }
 
 export type DigitOrInvalid = number | '?'
